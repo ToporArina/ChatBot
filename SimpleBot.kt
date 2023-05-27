7dc8c57fe35c92aa963d41c4b8ac81a82a395f00
l000ps <toporoleksandr@gmail.com>
2023-05-27 10:07:34 +0300

    Stage 4/5: Learning numbers

--- SimpleBot.kt
+++ SimpleBot.kt
@@ -16,7 +16,17 @@ fun main() {
     println("Let me guess your age.")
     println("Enter remainders of dividing your age by 3, 5 and 7.")
 
-    val yourAge = (scanner.nextInt() * 70 + scanner.nextInt() * 21 + scanner.nextInt() * 15) % 105
+    val rem3 = scanner.nextInt()
+    val rem5 = scanner.nextInt()
+    val rem7 = scanner.nextInt()
+    val age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
 
-    println("Your age is ${yourAge}; that's a good time to start programming!")
+    println("Your age is " + age + "; that's a good time to start programming!")
+    println("Now I will prove to you that I can count to any number you want.")
+
+    for (i in 0 .. scanner.nextInt()) {
+        println("$i!")
+    }
+
+    println("Completed, have a nice day!")
 }
